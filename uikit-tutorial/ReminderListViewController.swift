af5d0b41cf83ffdc89e12c0121843cf6f3203ff6
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-07-04 21:02:54 +0900

    Init: build project to UIKit

---
+++ uikit-tutorial/ReminderListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit-tutorial
+//
+//  Created by apple developer academy on 2022/07/04.
+//
+
+import UIKit
+
+class ReminderListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
